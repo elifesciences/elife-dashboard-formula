elifesciences/elife-dashboard-formula

commit 4c8168dd60be202fe2d8586850e2e75710c20ae0

removes dummy socket state

salt/elife-dashboard/uwsgi.sls
@@ -40,24 +40,21 @@ uwsgi-app-upstart:
         - source: salt://elife-dashboard/config/etc-init-uwsgi-app.conf
         - mode: 755
 
-{% if salt['grains.get']('osrelease') == "14.04" %}
-uwsgi-elife-dashboard.socket:
-    cmd.run:
-        - name: echo "dummy state"
-{% else %}
+{% if salt['grains.get']('osrelease') != "14.04" %}
 uwsgi-elife-dashboard.socket:
     service.running:
         - enable: True
+        - require_in:
+            - uwsgi-app
 {% endif %}
 
 uwsgi-app:
     service.running:
         {% if salt['grains.get']('osrelease') != "14.04" %}
-        - name: uwsgi-elife-dashboard
+        - name: uwsgi-elife-dashboard # newrelic is expecting 'uwsgi-app' (urgh)
         {% endif %}
         - enable: True
         - require:
-            - uwsgi-elife-dashboard.socket
             - uwsgi-app-upstart
             - uwsgi-params
             - app-uwsgi-conf

commit 3ac6b0f541cac045f605093469dbe4e8e25913a2

uwsgi.sls, fixes broken reverse requisite. broken in 2019 in revision 4c8168dd60be202fe2d8586850e2e75710c20ae0

salt/elife-dashboard/uwsgi.sls
@@ -31,14 +31,13 @@ app-uwsgi-conf:
 uwsgi-elife-dashboard.socket:
     service.running:
         - enable: True
-        - require_in:
-            - uwsgi-app
 
 uwsgi-elife-dashboard:
     service.running:
         - enable: True
         - require:
             - uwsgi-params
+            - uwsgi-elife-dashboard.socket
             - app-uwsgi-conf
             - app-nginx-conf
             - app-log-file